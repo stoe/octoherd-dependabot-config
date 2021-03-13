263def2f1212e787d1cb155d4ae169ea568c8483
Stefan Stölzle <stefan@stoelzle.me>
2021-03-13 10:52:40 +0100

    📦 Init

---
+++ readme.md
@@ -0,0 +1,30 @@
+# octoherd-script-dependabot-config
+
+> description
+
+[![ci](https://github.com/stoe/octoherd-script-dependabot-config/workflows/ci/badge.svg)](https://github.com/stoe/octoherd-script-dependabot-config/actions/workflows/ci.yml) [![publish](https://github.com/stoe/octoherd-script-dependabot-config/actions/workflows/publish.yml/badge.svg)](https://github.com/stoe/octoherd-script-dependabot-config/actions/workflows/publish.yml) [![styled with prettier](https://img.shields.io/badge/styled_with-prettier-ff69b4.svg)](https://github.com/prettier/prettier)
+
+## Usage
+
+```js
+npx octoherd-script-dependabot-config \
+  --octoherd-token 0123456789012345678901234567890123456789 \
+  "stoe/*"
+```
+
+## Options
+
+| option | type | description |
+| ------ | ---- | ----------- |
+
+## Contributing
+
+See [CONTRIBUTING.md](https://github.com/stoe/.github/blob/main/.github/CONTRIBUTING.md)
+
+## About Octoherd
+
+[@octoherd](https://github.com/octoherd/) is project to help you keep your GitHub repositories in line.
+
+## License
+
+[MIT](license)
---
+++ script.js
@@ -0,0 +1,103 @@
+import {readFileSync} from 'fs'
+import {resolve} from 'path'
+
+const pkg = JSON.parse(readFileSync('./package.json'))
+
+import {composeCreatePullRequest} from 'octokit-plugin-create-pull-request'
+
+/**
+ * @param {import('@octoherd/cli').Octokit} octokit
+ * @param {import('@octoherd/cli').Repository} repository
+ */
+export async function script(octokit, repository) {
+  if (repository.archived) {
+    octokit.log.info({change: false}, `repository archived`)
+    return
+  }
+
+  if (repository.fork) {
+    octokit.log.info({change: false}, `repository is a fork`)
+    return
+  }
+
+  // if (repository.size === 0) {
+  //   octokit.log.info({change: false}, `repository is empty`)
+  //   return
+  // }
+
+  const {
+    owner: {login: owner},
+    name: repo,
+    html_url
+  } = repository
+
+  const language = repository.language ? repository.language.toLowerCase() : null
+
+  let configPath = undefined
+  switch (language) {
+    case 'javascript':
+    case 'go':
+      configPath = resolve(`./dependabot.${language}.yml`)
+      break
+    case 'hcl':
+      configPath = resolve(`./dependabot.terraform.yml`)
+      break
+    default:
+      configPath = resolve(`./dependabot.default.yml`)
+      break
+  }
+
+  const newContent = readFileSync(configPath)
+
+  const payload = {
+    message: 'Add dependabot config',
+    content: newContent.toString('base64')
+  }
+
+  try {
+    const {
+      data: {content}
+    } = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+      owner,
+      repo,
+      path: '.github/dependabot.yml'
+    })
+    const buf = Buffer.from(content, 'base64')
+
+    if (buf.toString('utf-8') === newContent.toString('utf-8')) {
+      octokit.log.info(`${html_url} no change`)
+      return
+    }
+
+    payload.message = 'Update dependabot config'
+  } catch (error) {
+    // do nothing
+  }
+
+  try {
+    const {data: pr} = await composeCreatePullRequest(octokit, {
+      owner,
+      repo,
+      title: payload.message,
+      body: `via [${pkg.name}](${pkg.repository})`,
+      base: 'main',
+      head: 'dependabot-config',
+      changes: [
+        {
+          files: {
+            '.github/dependabot.yml': {
+              encoding: 'base64',
+              content: payload.content
+            }
+          },
+          commit: `🤖 ${payload.message}`,
+          emptyCommit: false
+        }
+      ],
+      createWhenEmpty: false
+    })
+    octokit.log.info(`pull request created ${pr.html_url}`)
+  } catch (error) {
+    octokit.log.error(`${error.message}`)
+  }
+}
